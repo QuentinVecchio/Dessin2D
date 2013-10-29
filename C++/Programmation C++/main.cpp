6070deb85153f53d8dd86d479d455f7464b7da6b
Matthieu CLIN <16208312+clinm@users.noreply.github.com>
2013-10-29 11:42:48 +0100

    ajout du dernier dossier "Quadrilatere"

--- C++/Programmation C++/main.cpp
+++ C++/Programmation C++/main.cpp
@@ -1,21 +1,10 @@
 #include <iostream>
 
-#include "./Triangle/Triangle.h"
-#include "./Triangle/TRectangle.h"
-#include "./Triangle/Isocele.h"
-#include "./Triangle/Equilateral.h"
+
 using namespace std;
 
 int main()
 {
-	Triangle t(Point2D(2,3),Point2D(3,3),Point2D(2,5));
 
-	cout << t<<endl;
-
-	TRectangle tr(Point2D(2,3),Point2D(3,3),Point2D(2,5));
-
-	cout <<tr << endl;
-	Isocele i (Point2D(2,3),Point2D(3,3),Point2D(2,5));
-	Equilateral e (Point2D(2,3),Point2D(3,3),Point2D(2,5));
 	return 0;
 }
