20429aa33034503f82d73d0dc690ca356d886fd5
Matthieu CLIN <16208312+clinm@users.noreply.github.com>
2013-10-29 11:57:41 +0100

    les constructeurs fonctionnent

--- C++/Programmation C++/main.cpp
+++ C++/Programmation C++/main.cpp
@@ -10,6 +10,22 @@ using namespace std;
 
 int main()
 {
+	Carre c(Point2D(2,3), Point2D(3,2), Point2D(3,3), Point2D(2,2));
+	cout << c << endl;
 
+	Losange l(Point2D(2,3), Point2D(3,2), Point2D(3,3), Point2D(2,2));
+	cout << l << endl;
+
+	Quadrilatere qa(Point2D(2,3), Point2D(3,2), Point2D(3,3), Point2D(2,2));
+	cout << qa << endl;
+
+	Parallelogramme p(Point2D(2,3), Point2D(3,2), Point2D(3,3), Point2D(2,2));
+	cout << p << endl;
+
+	Quadrangle q(Point2D(2,3), Point2D(3,2), Point2D(3,3), Point2D(2,2));
+	cout << q << endl;
+
+	Rectangle r(Point2D(2,3), Point2D(3,2), Point2D(3,3), Point2D(2,2));
+	cout << r << endl;
 	return 0;
 }
