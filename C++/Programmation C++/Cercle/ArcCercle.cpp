13dbb4f55380ac38e08ffa9f6984c1f1cde7c58b
Matthieu CLIN <16208312+clinm@users.noreply.github.com>
2013-11-15 09:53:22 +0100

    ArcCercle

--- C++/Programmation C++/Cercle/ArcCercle.cpp
+++ C++/Programmation C++/Cercle/ArcCercle.cpp
@@ -1,2 +1,27 @@
+/**
+*       @Author Clin Matthieu
+*       @Author Vecchio Quentin
+*
+*       @version 1.0.0.0 on 2013-11-15
+*/
 #include "ArcCercle.h"
+
+	// :Cercle(cercle),angle(angle),pointDepart(pointDepart){}
+        ArcCercle::ArcCercle(Cercle cercle, float angle ,Point2D pointDepart){}
+
+        float ArcCercle::getAngle(){
+		return this->angle;
+	}
+
+        Point2D ArcCercle::getPointDepart(){
+		return this->pointDepart;
+	}
+
+        void ArcCercle::setAngle(float angleA){
+		this->angle = angleA;
+	}
+        
+        void ArcCercle::setPointDepart(Point2D pointDepart){
+		this->pointDepart = pointDepart;
+	}
 
