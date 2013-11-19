dfba561722274354395f7399b91dc09ce6af1579
Matthieu CLIN <16208312+clinm@users.noreply.github.com>
2013-11-19 11:30:31 +0100

    fin des commentaires

--- C++/Programmation C++/Triangle/Triangle.h
+++ C++/Programmation C++/Triangle/Triangle.h
@@ -1,3 +1,9 @@
+/**
+*	@Author Clin Matthieu
+*	@Author Vecchio Quentin
+*
+*	@version 1.0.0.0 on 2013-11-15
+*/
 #ifndef Triangle_h
 #define Triangle_h
 
@@ -11,10 +17,25 @@ class Triangle : public Segment {
 
  public:
 
+
+	/**
+	*	Constructeur
+	*	@params Point2D A, le point A du triangle
+	*	@params Point2D B, le point B du triangle
+	*	@params Point2D C, le point C du triangle
+	*/
     Triangle(Point2D A, Point2D B, Point2D C);
 
+	/**
+	*	Constructeur
+	*	@params Segment base, le segment base du triangle
+	*	@params Point2D C, le point C du triangle
+	*/
     Triangle(Segment base, Point2D C);
 
+	/**
+	*	Getter et Setter du point C
+	*/
     Point2D getC();
 
     void setC(Point2D C);
@@ -23,6 +44,11 @@ class Triangle : public Segment {
 
     void translation(Point2D point);
 
+	/**
+	*	Fonction qui test si le triangle est plat
+	*	@return true, si il est plat
+	*	@return false, sinon
+	*/
     bool estPlat();
 
  protected:
